1b4e07959be0bf52d3dacb2b6bd731f02b11c109
Abdul Kholik <akl859349@gmail.com>
2025-11-14 13:03:34 +0700

    update versi php

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -36,7 +36,6 @@ class ComposerStaticInitd5a4b1cdd7f3c5d6ecf1b26863504111
         ),
         'M' => 
         array (
-            'MyCLabs\\Enum\\' => 13,
             'Matrix\\' => 7,
             'Masterminds\\' => 12,
         ),
@@ -93,10 +92,6 @@ class ComposerStaticInitd5a4b1cdd7f3c5d6ecf1b26863504111
         array (
             0 => __DIR__ . '/..' . '/phpoffice/phpspreadsheet/src/PhpSpreadsheet',
         ),
-        'MyCLabs\\Enum\\' => 
-        array (
-            0 => __DIR__ . '/..' . '/myclabs/php-enum/src',
-        ),
         'Matrix\\' => 
         array (
             0 => __DIR__ . '/..' . '/markbaker/matrix/classes/src',
@@ -140,7 +135,6 @@ class ComposerStaticInitd5a4b1cdd7f3c5d6ecf1b26863504111
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
         'Dompdf\\Cpdf' => __DIR__ . '/..' . '/dompdf/dompdf/lib/Cpdf.php',
-        'Stringable' => __DIR__ . '/..' . '/myclabs/php-enum/stubs/Stringable.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
