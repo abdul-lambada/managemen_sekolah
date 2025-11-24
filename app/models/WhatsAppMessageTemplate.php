60a3e35ed8d24892b991cd7265a15bc89891a03d
Abdul Kholik <akl859349@gmail.com>
2025-11-24 15:13:56 +0700

    update code

--- app/models/WhatsAppMessageTemplate.php
+++ app/models/WhatsAppMessageTemplate.php
@@ -1,13 +1,11 @@
 <?php
 
-declare(strict_types=1);
-
 class WhatsAppMessageTemplate extends Model
 {
-    protected string $table = 'whatsapp_message_templates';
-    protected string $primaryKey = 'id';
+    protected $table = 'whatsapp_message_templates';
+    protected $primaryKey = 'id';
 
-    public function activeTemplates(): array
+    public function activeTemplates()
     {
         $stmt = $this->db->query("SELECT * FROM whatsapp_message_templates WHERE is_active = 1 ORDER BY name");
         return $stmt->fetchAll();
