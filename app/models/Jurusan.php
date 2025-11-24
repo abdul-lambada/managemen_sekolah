b63c31def65b2f625fc1bce7aaec010a44d03b49
Abdul Kholik <akl859349@gmail.com>
2025-11-24 10:28:18 +0700

    update model jurusan to version php 7

--- app/models/Jurusan.php
+++ app/models/Jurusan.php
@@ -1,13 +1,11 @@
 <?php
 
-declare(strict_types=1);
-
 class Jurusan extends Model
 {
-    protected string $table = 'jurusan';
-    protected string $primaryKey = 'id_jurusan';
+    protected $table = 'jurusan';
+    protected $primaryKey = 'id_jurusan';
 
-    public function options(): array
+    public function options()
     {
         return $this->db->query("SELECT id_jurusan, nama_jurusan FROM jurusan ORDER BY nama_jurusan")->fetchAll();
     }
