789cedd64fbb35df5e745b31fd0f0b341aba55aa
Abdul Kholik <akl859349@gmail.com>
2025-11-24 15:07:36 +0700

    update code

--- app/models/FingerprintLog.php
+++ app/models/FingerprintLog.php
@@ -1,13 +1,11 @@
 <?php
 
-declare(strict_types=1);
-
 class FingerprintLog extends Model
 {
-    protected string $table = 'fingerprint_logs';
-    protected string $primaryKey = 'id';
+    protected $table = 'fingerprint_logs';
+    protected $primaryKey = 'id';
 
-    public function recent(int $limit = 50): array
+    public function recent($limit = 50)
     {
         $stmt = $this->db->prepare("SELECT * FROM fingerprint_logs ORDER BY created_at DESC LIMIT :limit");
         $stmt->bindValue(':limit', $limit, PDO::PARAM_INT);
