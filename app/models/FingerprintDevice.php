132d35438a292caa6458d4f9aa50c4b73c7ddbb0
Abdul Kholik <akl859349@gmail.com>
2025-11-24 15:06:09 +0700

    update code

--- app/models/FingerprintDevice.php
+++ app/models/FingerprintDevice.php
@@ -1,13 +1,11 @@
 <?php
 
-declare(strict_types=1);
-
 class FingerprintDevice extends Model
 {
-    protected string $table = 'fingerprint_devices';
-    protected string $primaryKey = 'id';
+    protected $table = 'fingerprint_devices';
+    protected $primaryKey = 'id';
 
-    public function active(): array
+    public function active()
     {
         $stmt = $this->db->query("SELECT * FROM fingerprint_devices WHERE is_active = 1 ORDER BY nama_lokasi");
         return $stmt->fetchAll();
