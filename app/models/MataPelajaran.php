c2005273ccaf9132928f7288337b404fc367efd8
Abdul Kholik <akl859349@gmail.com>
2025-11-24 10:37:12 +0700

    update code Model Mata pelajaran to versio php 7

--- app/models/MataPelajaran.php
+++ app/models/MataPelajaran.php
@@ -1,13 +1,11 @@
 <?php
 
-declare(strict_types=1);
-
 final class MataPelajaran extends Model
 {
-    protected string $table = 'mata_pelajaran';
-    protected string $primaryKey = 'id_mata_pelajaran';
+    protected $table = 'mata_pelajaran';
+    protected $primaryKey = 'id_mata_pelajaran';
 
-    public function options(): array
+    public function options()
     {
         $sql = "SELECT id_mata_pelajaran, kode_mapel, nama_mapel FROM mata_pelajaran ORDER BY nama_mapel";
         return $this->db->query($sql)->fetchAll();
